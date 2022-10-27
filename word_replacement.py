31897a7acc74a34ed36085f2de71b3267cdc7e4d
RyoCodes <100488539+RyoCodes@users.noreply.github.com>
2022-10-27 15:53:15 +0530

    Add files via upload
    a simple word replacement program.

---
+++ word_replacement.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 14:45:36 2022
+
+@author: VARUN
+"""
+
+def replace_word():
+    
+    str = "Hi guys, I am Vyshhnavi and hi hi hi"
+    word_to_replace = input("Enter the word you want to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+    
+replace_word()
+    
